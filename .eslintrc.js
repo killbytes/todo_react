96090cee92fed4edd56e7fce578de580a3fdf671
rrain <rrainpath@gmail.com>
2024-03-17 09:45:34 +0800

    applied prettier for .eslint.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,78 +1,73 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
-  ignorePatterns: ["node_modules", "dist", "build"],
+  ignorePatterns: ['node_modules', 'dist', 'build'],
   extends: [
     'airbnb',
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
     'plugin:prettier/recommended',
-    "plugin:import/errors",
-    "plugin:import/warnings"
+    'plugin:import/errors',
+    'plugin:import/warnings',
   ],
   overrides: [
     {
       env: {
-        node: true
+        node: true,
       },
       files: ['.eslintrc.{js,cjs,ts,cts}'],
       parserOptions: {
-        sourceType: 'script'
-      }
-    }
+        sourceType: 'script',
+      },
+    },
   ],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
     ecmaFeatures: {
-      "jsx": true
-    }
+      jsx: true,
+    },
   },
-plugins: [
-    'react',
-    'prettier',
-    'import',
-    'jsx-a11y'
-  ],
-parser: '@typescript-eslint/parser',
-ecmaFeatures: {
-  jsx: true
-},
+  plugins: ['react', 'prettier', 'import', 'jsx-a11y'],
+  parser: '@typescript-eslint/parser',
+  ecmaFeatures: {
+    jsx: true,
+  },
   rules: {
     'prettier/prettier': [
       'error',
       {
-        endOfLine: 'auto'
-      }
+        endOfLine: 'auto',
+      },
     ],
     'react/react-in-jsx-scope': 'off',
-    'react/jsx-filename-extension': [2, { 'extensions': ['.js', '.jsx', '.ts', '.tsx'] }],
-    "react/destructuring-assignment": 'off',
-      "import/extensions": [
-      "error",
-      "ignorePackages",
+    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    'react/destructuring-assignment': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
       {
-        "js": "never",
-        "jsx": "never",
-        "ts": "never",
-        "tsx": "never"
-      }
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
     ],
-    "import/no-unresolved": 'off',
-    "no-shadow": 'off',
-    "jsx-a11y/label-has-associated-control": 'off',
-    "jsx-a11y/control-has-associated-label": 'off',
-    "react/sort-comp": 'off',
-    "class-methods-use-this": 'off',
+    'import/no-unresolved': 'off',
+    'no-shadow': 'off',
+    'jsx-a11y/label-has-associated-control': 'off',
+    'jsx-a11y/control-has-associated-label': 'off',
+    'react/sort-comp': 'off',
+    'class-methods-use-this': 'off',
   },
   settings: {
     'import/resolver': {
       node: {
-        extensions: [".js", ".jsx", ".ts", ".tsx"]
-      }
-    }
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
   },
 };
